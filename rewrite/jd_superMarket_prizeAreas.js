f0f73ce50e53ab1ae0e2513da5095d0cffee809a
taoing <529982750@qq.com>
2021-06-03 14:35:40 +0800

    update

---
+++ rewrite/jd_superMarket_prizeAreas.js
@@ -0,0 +1,27 @@
+/*
+东东超市兑换列表重写
+ */
+
+let body = $response.body;
+let data = JSON.parse(body);
+if (data.data.bizCode !== 0) {
+    console.log(data.data.bizMsg);
+    $done();
+}
+let result = data.data.result;
+let prizeAreas = result.areas;
+for (let i = 0; i < prizeAreas.length; i++) {
+    let area = prizeAreas[i];
+    if (area.areaId === 6) {
+        area.type = 0;
+        let prizes = area.prizes;
+        for (let j = 0; j < prizes.length; j++) {
+            prizes[j].type = 2;
+            prizes[j].status = 0;
+        }
+        break;
+    }
+}
+body = JSON.stringify(data);
+console.log("modify body:\n" + body);
+$done(body);
