41625bd900e3c9664417588b40d5b5a0e79551d9
taoing <529982750@qq.com>
2022-04-02 17:28:48 +0800

    Add files via upload

---
+++ rewrite/ciyuanji_user_account.js
@@ -0,0 +1,22 @@
+/*
+次元姬app 用户金币重写
+
+QuantumultX:
+[MITM]
+hostname = api.huowenkeji.com
+
+^https://api.huowenkeji.com/api/ciyuanji/client/account/getAccountByUser.+ url script-response-body https://raw.githubusercontent.com/taoing/quantumultX/master/rewrite/ciyuanji_user_account.js
+
+ */
+
+let body = $response.body;
+let obj = JSON.parse(body);
+if (obj.code !== "200") {
+  $done();
+} else {
+  if (obj.data && obj.data.accountInfo) {
+    obj.data.accountInfo.couponBalance = 600;
+    body = JSON.stringify(obj);
+    $done(body);
+  }
+}
